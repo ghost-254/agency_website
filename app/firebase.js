ae8ef5fdf898d2e5c4a292f064bda08e27d6c238
ghost-254 <nevilleotieno6@gmail.com>
2024-08-04 12:43:57 -0600

    new

--- app/firebase.js
+++ app/firebase.js
@@ -4,24 +4,21 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDPAxC2BXjeXdy5Agt6a6Y-kzzavuV8Ywc",
-  authDomain: "outreachconnect-b4e18.firebaseapp.com",
-  projectId: "outreachconnect-b4e18",
-  storageBucket: "outreachconnect-b4e18.appspot.com",
-  messagingSenderId: "966975707949",
-  appId: "1:966975707949:web:b955e080bd4a0037dd51a4",
-  measurementId: "G-FLZRJ90LXX"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const analytics = typeof window !== 'undefined' ? getAnalytics(app) : null;
 const auth = getAuth(app);
 
 export { auth };
